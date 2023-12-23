42eda42b996fc1ccd2d0cab3e108e80a536ed982
Otabek <mavlonovo@tamu.edu>
2023-12-22 21:26:43 -0600

    added POST and DELETE handlers

--- index.js
+++ index.js
@@ -36,12 +36,8 @@ app.post("/get-secret", async (req, res) => {
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
   // const searchId = req.body.id;
-  const secretName = req.body.secret;
-  const secretScore = req.body.score;
-
   try {
-    const result = await axios.get(API_URL + "/secrets/" + searchId, config);
-
+    const result = await axios.post(API_URL + "/secrets/", config);
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
     res.render("index.ejs", { content: JSON.stringify(error.response.data) });
@@ -79,12 +75,19 @@ app.post("/patch-secret", async (req, res) => {
   } catch (error) {
     res.render("index.ejs", { content: JSON.stringify(error.response.data) });
   }
-
 });
 
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+
+  try {
+    const result = await axios.delete(API_URL + "/secrets/" + searchId, config);
+
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.listen(port, () => {
